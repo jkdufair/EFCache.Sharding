5555e468798634296b0fd1d6af09e8e65d04bb72
Jason Dufair <jase@purdue.edu>
2018-08-24 16:16:10 -0400

    Make references relative to solution

--- EFCache.Sharding/Properties/AssemblyInfo.cs
+++ EFCache.Sharding/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EFCache.Sharding")]
+[assembly: AssemblyTitle("Blarp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EFCache.Sharding")]
+[assembly: AssemblyProduct("Blarp")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e1530cec-dd14-4597-bb42-b65a4ff59f57")]
+[assembly: Guid("96d24213-7b0e-4b10-b3ee-f807f200b35a")]
 
 // Version information for an assembly consists of the following four values:
 //
